2734a0c460b8c9b90617db492da54a018a9c0cdc
Gregg Carey <gregg.carey@outlook.com>
2022-10-14 14:45:00 -0400

    BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+//componets
+import BookList from "./components/BookList";
+
 function App() {
   return (
     <div id="main">
       <h1>Does this work now?</h1>
+      <BookList />
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList() {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book Name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
